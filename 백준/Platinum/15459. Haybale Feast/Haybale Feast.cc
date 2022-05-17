b85b4b45627f14e8607d75becd61031c583c2bf6
jihyuk1023 <59600347+jihyuk1023@users.noreply.github.com>
2022-05-17 19:09:22 +0900

    [Platinum V] Title: Haybale Feast, Time: 28 ms, Memory: 3588 KB -BaekjoonHub

--- 백준/Platinum/15459. Haybale Feast/Haybale Feast.cc
+++ 백준/Platinum/15459. Haybale Feast/Haybale Feast.cc
@@ -7,73 +7,58 @@ using namespace std;
 #define debug(x)  std::cout << "[Debug] " << #x << " is " << x << '\n'
 #define endl '\n'
 typedef long long ll;
-typedef pair<ll, ll> Pair;
 
-Pair operator+(Pair& p1, Pair& p2){
-    return {p1.first + p2.first, p1.second + p2.second};
+ll N, M;
+vector<ll> flavor, spiciness;
+
+/**
+ * @brief Parametric search로 조건을 만족하는 매운맛 허들을 찾기위해 이분탐색함
+ * 
+ * 조건1. 연속인 열일 것
+ * 조건2. flavor의 합이 M 이상일 것
+ * 
+ * 문제: 조건을 만족하는 열의 최대 매운맛 중 최솟값을 찾을 것.
+ * 조건을 확인하는 함수를 Maximum Subarray(최대 부분배열합) 문제와 비슷하게 구현한다.
+ */
+
+bool check(ll spicyHurdle){
+    ll contiguousFlavorSum = 0;
+    for(ll i = 0; i < N; i++){
+        if(spiciness[i] > spicyHurdle){
+            // 매운맛 조건을 못 만족시켜 열이 끊어졌다면 연속합을 초기화 한다.
+            contiguousFlavorSum = 0;
+        }else{
+            contiguousFlavorSum += flavor[i];
+
+            // 조건을 만족한다면 바로 리턴한다.
+            if(contiguousFlavorSum >= M) return true;
+        }
+    }
+    return false;
 }
-
-Pair operator+=(Pair& p1, Pair& p2){
-    return p1 = p1 + p2;
-}
-
-struct UnionFind{
-    vector<Pair> parent; // {parent, flavor}
-    UnionFind(ll n, vector<ll> flavors){
-        parent.resize(n + 1);
-        
-        for(ll i = 0; i < n; i++)
-            parent[i] = {-1, flavors[i]};
-    }
-    // find root
-    ll find(ll n){
-        if(parent[n].first < 0) return n;
-        return parent[n].first = find(parent[n].first); 
-    }
-    void merge(ll a, ll b){
-        a = find(a);
-        b = find(b);
-        if(a == b) return;
-
-        parent[a] += parent[b];
-        parent[b].first = a;
-    }
-    ll getFlavorSum(ll n){
-        return parent[find(n)].second;
-    }
-};
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-
-    ll N, M;
+    
     cin >> N >> M;
+    flavor.resize(N); 
+    spiciness.resize(N);
 
-    vector<ll> flavors(N), spiciness(N);
-    vector<Pair> index(N);
-
+    ll lo = 0, hi = 0;
     for(ll i = 0; i < N; i++){
-        cin >> flavors[i] >> spiciness[i];
-        index[i] = {spiciness[i], i};
+        cin >> flavor[i] >> spiciness[i];
+        hi = max(hi, spiciness[i]);
     }
 
-    UnionFind uf(N, flavors);
-    sort(index.begin(), index.end());
-
-    for(Pair pair: index){
-        ll index = pair.second;
-        ll left = (index - 1 >= 0)? index - 1 : 0;
-        ll right = (index + 1 < N)? index + 1 : N-1;
-
-        if(spiciness[left] <= spiciness[index])
-            uf.merge(left, index);
-        if(spiciness[index] >= spiciness[right])
-            uf.merge(index, right);
-        if(uf.getFlavorSum(index) >= M){
-            cout << spiciness[index];
-            break;
-        }
+    while(lo + 1 < hi){
+        ll mid = (lo + hi)/2;
+        
+        if(check(mid)) hi = mid;
+        else lo = mid;
     }
+    cout << hi;
+
+
     return 0;
 }
