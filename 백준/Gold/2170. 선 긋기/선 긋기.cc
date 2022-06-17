575c72f2760d9d12107ac64b5b832514785f42c1
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-06-17 14:18:31 +0900

    [Gold V] Title: 선 긋기, Time: 364 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.cc
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -10,35 +10,40 @@ typedef long long ll;
 typedef pair<int, int> P;
 const int INF = 1e9 + 1;
 
+class solve{
+public:
+    ll lineSweeping(int n, vector<P>& arr){
+        sort(arr.begin(), arr.end());
+        
+        ll ret = 0;
+        int l = -INF, r = -INF;
+        for(P line: arr){
+            if(r < line.first){
+                ret += (r - l);
+                l = line.first;
+                r = line.second;
+            }else
+                r = max(r, line.second);
+        }
+
+        ret += (r - l);
+        return ret;
+    }
+};
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    
+
     int N;
     cin >> N;
-    vector<P> lines(N);
 
+    vector<P> arr(N);
     for(int i = 0; i < N; i++)
-        cin >> lines[i].first >> lines[i].second;
+        cin >> arr[i].first >> arr[i].second;
 
-    sort(lines.begin(), lines.end());
-    // [l, r] : 현재 합치고 있는 구간
-    int res = 0, l = -INF, r = -INF;
-    for(int i = 0; i < N; i++){
-        // 아예 새로운 선분을 만나면
-        if(r < lines[i].first){
-            // 이전 선분의 길이를 더하고
-            res += (r - l);
-
-            // 현재의 새로운 선분의 왼쪽 끝과 오른쪽 끝을 기억한다.
-            l = lines[i].first;
-            r = lines[i].second;
-        }else 
-            r = max(r, lines[i].second);
-            // 연속된 선분인 경우, 오른쪽 끝만 계속 늘려간다.
-    }
-    res += (r - l); // 마지막 선분까지 더해줌.
-    cout << res;
-
+    solve sol;
+    cout << sol.lineSweeping(N, arr);
+    
     return 0;
 }
