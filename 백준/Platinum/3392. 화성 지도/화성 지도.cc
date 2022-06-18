08ce40e6c5cccaa07bbe612f64abda6a0ea8668a
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-06-18 20:32:30 +0900

    [Platinum II] Title: 화성 지도, Time: 604 ms, Memory: 3172 KB -BaekjoonHub

--- 백준/Platinum/3392. 화성 지도/화성 지도.cc
+++ 백준/Platinum/3392. 화성 지도/화성 지도.cc
@@ -11,11 +11,14 @@ typedef long long ll;
 struct Point{
     int x, y1, y2;
     bool start;
+    Point(int _x, int _y1, int _y2, bool _start){
+        x = _x, y1 = _y1, y2 = _y2, start = _start;
+    }
 };
 
 struct SegTree{
     int size;
-    vector<int> count; // count is Binary Search Tree used to mark all the Y points in the range
+    vector<int> count; // count is used for marking all the Y points in the range
     vector<int> segtree;
     SegTree(int n){
         size = 1;
@@ -24,23 +27,27 @@ struct SegTree{
         count.resize(size);
         segtree.resize(size);
     }
+
     // update count by marking Y region
-    void update(int s, int e, int val){
-        int l = s + size/2 - 1;
-        int r = e + size/2 - 1;
+    void mark_count(int s, int e, int val){
+        int l = s + size/2 - 1; // y를 one base index로 관리한다.
+        int r = e + size/2 - 1; 
 
         for(int i = l; i <= r; i++)
             count[i] += val;   
     }
-    // insert segtree with Y region
-    void insert(int s, int e, int val = 1){
-        int l = s + size/2 - 1;
+
+    // construct segtree with Y region
+    void construct(int s, int e, int val = 1){
+        int l = s + size/2 - 1; // y를 one base index로 관리한다.
         int r = e + size/2 - 1;
 
+        // leaf node initialization
         for(int i = l; i <= r; i++)
             if(count[i]) segtree[i] = val;
             else segtree[i] = 0;
 
+        // construct segtree in O(n)
         for(int i = size/2-1; i > 0; i--)
             segtree[i] = segtree[i*2] + segtree[i*2 + 1];
     } 
@@ -57,12 +64,9 @@ int main(){
         int x1, x2, y1, y2;
         cin >> x1 >> y1 >> x2 >> y2;
 
-        Point temp;
-        temp.y1 = y1 + 1, temp.y2 = y2 + 1;
-        temp.x = x1, temp.start = true;
-        points.push_back(temp);
-        temp.x = x2, temp.start = false;
-        points.push_back(temp);
+        // y를 one base index로 관리한다.
+        points.push_back(Point(x1, y1+1, y2+1, true));
+        points.push_back(Point(x2, y1+1, y2+1, false));
     }
 
     sort(points.begin(), points.end(), [](Point& a, Point& b) {
@@ -74,8 +78,8 @@ int main(){
     for(int i = 0; i < points.size(); i++){
         if(i > 0)
             answer += (points[i].x - points[i-1].x) * ST.segtree[1];
-        ST.update(points[i].y1, points[i].y2 - 1, points[i].start ? 1 : -1);      
-        ST.insert(points[i].y1, points[i].y2 - 1);
+        ST.mark_count(points[i].y1, points[i].y2 - 1, points[i].start ? 1 : -1);      
+        ST.construct(points[i].y1, points[i].y2 - 1);
     }
 
     cout << answer;
