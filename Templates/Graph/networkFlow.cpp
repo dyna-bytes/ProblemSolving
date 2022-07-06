3855c71ffb1ad98f5d46eed3748886636d564f5c
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-07-06 16:16:26 +0900

    Add files via upload
    Edmonds-Karp algorithm

---
+++ Templates/Graph/networkFlow.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <climits>
+using namespace std;
+#define debug(x)  std::cout << "[Debug] " << #x << " is " << x << '\n'
+#define endl '\n'
+typedef long long ll;
+
+// Edmonds-Karp algorithm
+const int INF = 1e9;
+
+struct Edge{
+    int to, c, f; // destination, capacity, flow
+    Edge* dual; // points the dual Edge of this one
+    Edge(){ 
+        Edge(-1, 0); 
+    }
+    Edge(int to1, int c1){
+        to = to1, c = c1, f = 0;
+        dual = nullptr;
+    }
+    int residual(){
+        return c - f;
+    }
+    void addFlow(int f1){
+        f += f1;
+        dual->f -= f1;
+    }
+};
+
+struct NetworkFlow{
+    int N;
+    vector<vector<Edge*>> adj;
+
+    NetworkFlow(int n){
+        N = n;
+        adj.resize(N + 1);
+    }
+
+    void addEdge(int from, int to, int cap){
+        Edge* e1 = new Edge(to, cap);
+        Edge* e2 = new Edge(from, 0);
+        e1->dual = e2;
+        e2->dual = e1;
+        adj[from].push_back(e1);
+        adj[to].push_back(e2);
+    }
+
+    int run(int start = 1, int end = 2){
+        int total = 0, S = start, E = end;
+
+        while(true){
+            vector<Edge*> edges(N + 1);
+            vector<int> prev(N + 1, -1);     
+            queue<int> q;
+            q.push(S);
+
+            while(!q.empty()){
+                int curr = q.front(); q.pop();
+                if(curr == E) break; // if reached end, stop
+
+                for(Edge* e: adj[curr]){ // e: curr-->next
+                    int next = e->to;
+                    // if visited next, continue
+                    if(prev[next] != -1) continue;
+                    if(e->residual() > 0){
+                        edges[next] = e;
+                        prev[next] = curr;
+                        q.push(next);
+
+                        if(next == E) break;
+                    }
+                }
+            }
+
+            // if there is no path to reach the end, done.
+            if(prev[E] == -1) break;     
+
+            int flow = INF; 
+            // get the minimum flow through a path
+            for(int node = E; node != S; node = prev[node])
+                flow = min(flow, edges[node]->residual());
+            // add flow to the path
+            for(int node = E; node != S; node = prev[node])
+                edges[node]->addFlow(flow);
+
+            total += flow; // add up to the total flow
+        }
+        return total;
+    }
+
+    void debugAdj(){
+        for(int node = 0; node < adj.size(); node++){
+            for(int next = 0; next < adj[node].size(); next++){
+                cout << "(" << node << ", " << adj[node][next]->to << ") ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    // Usage example
+    // problem BOJ 17412: https://www.acmicpc.net/problem/17412
+
+    int N, P; cin >> N >> P;
+    NetworkFlow Graph(N);
+
+    for(int i = 0; i < P; i++){
+        int from, to; cin >> from >> to;
+        Graph.addEdge(from, to, 1); // capa가 1인 단방향 간선 추가
+    }
+
+    cout << Graph.run();
+    
+    return 0;
+}
