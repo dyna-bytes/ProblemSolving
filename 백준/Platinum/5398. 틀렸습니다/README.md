00bb05c9412a7f501e3ef3c0ad5bb704297790a3
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-07-08 00:43:06 +0900

    [Platinum I] Title: 틀렸습니다, Time: 676 ms, Memory: 33968 KB -BaekjoonHub

---
+++ 백준/Platinum/5398. 틀렸습니다/README.md
@@ -0,0 +1,12 @@
+# [Platinum I] 틀렸습니다 - 5398 
+
+[문제 링크](https://www.acmicpc.net/problem/5398) 
+
+### 성능 요약
+
+메모리: 33968 KB, 시간: 676 ms
+
+### 분류
+
+이분 매칭(bipartite_matching)
+
---
+++ 백준/Platinum/5398. 틀렸습니다/틀렸습니다.cc
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x)  std::cout << "[Debug] " << #x << " is " << x << '\n'
+#define debugVec(v) do { \
+    std::cout << "[Debug] ["; \
+    for(int i = 0; i < ((v.size())-1); i++) std::cout << v[i] << "|"; \
+    std::cout << v[((v.size())-1)] << "]\n"; \
+} while(0)
+#define debugV2D(v) do { \
+    std::cout << "[Debug] [\n"; \
+    for(int y = 0; y < (v.size()); y++) { \
+        std::cout << "  ["; \
+        for(int x = 0; x < ((v[y].size())-1); x++) \
+            std::cout << v[y][x] << "|"; \
+        std::cout << v[y][(v[y].size())-1] << "]\n"; \
+    } \
+    std::cout << "]\n"; \
+} while(0)
+#define endl '\n'
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<int, char> pic;
+
+const int SIZE = 2002;
+#define key first
+#define value second
+
+struct MaxMatching{
+    int N, M;
+    vector<int>A, B;
+    vector<vector<int>> adj;
+    vector<int> visited;
+    MaxMatching(int n, int m){
+        N = n, M = m;
+        A.resize(N+1, -1), B.resize(M+1, -1);
+        adj.resize(N+1);
+        visited.resize(N+1);
+    }
+    void addAdj(int from, int to){
+        adj[from].push_back(to);
+    }
+    void debugAdj(){
+        cout << endl;
+        for (int n = 0; n < adj.size(); n++) {
+            cout << n << "->|";
+            for (int m = 0; m < adj[n].size(); m++)
+                cout << adj[n][m] << "|";
+            cout << endl;
+        }
+    }
+    bool canMatch(int a){
+        visited[a] = true;
+        for (int b: adj[a]) {
+            if (B[b] == -1 || !visited[B[b]] && canMatch(B[b])) {
+                A[a] = b;
+                B[b] = a;
+                return true;
+            }
+        }
+        return false;
+    }
+    int run(){
+        int match = 0;
+        for(int a = 0; a < N; a++){
+            if (A[a] == -1){
+                fill(visited.begin(), visited.end(), 0);
+                if (canMatch(a)) match++;
+            }
+        }
+        return match;
+    }
+};
+
+void solve(){
+    vector<vector<pic>> hmap(SIZE, vector<pic>(SIZE));
+    int H, V; cin >> H >> V;
+    MaxMatching Graph(H, V);
+
+    for (int h = 0; h < H; h++) {
+        int sx, sy; cin >> sx >> sy;
+        string word; cin >> word;
+        for (int x = 0; x < word.size(); x++)
+            hmap[sy][sx + x] = {h, word[x]};
+    }
+
+    for (int v = 0; v < V; v++) {
+        int sx, sy; cin >> sx >> sy;
+        string word; cin >> word;
+        for (int y = 0; y < word.size(); y++){
+            if (hmap[sy + y][sx].value 
+            && (hmap[sy + y][sx].value != word[y]))
+                Graph.addAdj(hmap[sy + y][sx].key, v);
+        }
+    }
+
+    cout << H + V - Graph.run() << endl;
+    // Graph.debugAdj();
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int T; cin >> T;
+    while(T--){
+        solve();
+    }
+    
+    return 0;
+}
