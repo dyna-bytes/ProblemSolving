47d285ba204660adf3a4eed0e8b719574ab2ec9d
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-08-22 21:13:53 +0900

    [Silver IV] Title: ∑|ΔEasyMAX|, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17203. ∑｜ΔEasyMAX｜/∑｜ΔEasyMAX｜.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x)  std::cout << "[Debug] " << #x << " is " << x << '\n'
+#define debugVec(v) do { \
+    std::cout << "[Debug] ["; \
+    for(int i = 0; i < ((v.size())-1); i++) std::cout << v[i] << "|"; \
+    std::cout << v[((v.size())-1)] << "]\n"; \
+} while(0)
+#define debugV2D(v) do { \
+    std::cout << "[Debug] [\n"; \
+    for(int y = 0; y < (v.size()); y++) { \
+        if(v[y].empty()){ std::cout << "  []\n"; continue; } \
+        std::cout << "  ["; \
+        for(int x = 0; x < ((v[y].size())-1); x++) \
+            std::cout << v[y][x] << "|"; \
+        std::cout << v[y][(v[y].size())-1] << "]\n"; \
+    } \
+    std::cout << "]\n"; \
+} while(0)
+#define endl '\n'
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vint;
+typedef vector<pii> vpii;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int N, Q;
+    cin >> N >> Q;
+    vint A(N);
+    vint pSum(N+1);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+        if (i == 0) continue;
+        pSum[i+1] = pSum[i] + abs(A[i] - A[i-1]);
+    }
+
+    for (int i = 0; i < Q; i++) {
+        int s, e; cin >> s >> e;
+        if (e <= s) cout << 0 << endl;
+        else cout << pSum[e] - pSum[s] << endl;
+    }
+    // debugVec(pSum);
+    return 0;
+}
