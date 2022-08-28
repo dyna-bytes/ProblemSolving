e989e31a697457948343b7a852bc183f5adff38c
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-08-29 00:18:07 +0900

    [Platinum II] Title: 최대 증가 직사각형 집합, Time: 72 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Platinum/1666. 최대 증가 직사각형 집합/최대 증가 직사각형 집합.cc
+++ 백준/Platinum/1666. 최대 증가 직사각형 집합/최대 증가 직사각형 집합.cc
@@ -17,27 +17,12 @@ using namespace std;
     } \
     std::cout << "]\n"; \
 } while(0)
-#define endl '\n'
+#define startl '\n'
 typedef long long ll;
 typedef pair<int, int> pii;
-typedef tuple<int, int, int> tiii;
 typedef vector<int> vint;
 typedef vector<pii> vpii;
-typedef vector<tiii> vtiii;
 
-/**
- * @brief 세그먼트 트리를 이용한 DP 최적화
- * 
- * dp[i] = 0~i번 째 직사각형 중 최대 증가를 만족하는 최대 직사각형들의 개수
- * dp[i] = x[i].s (i번째 직사각형의 시작점 x좌표) > x[i'].e (i'번째 직사각형의 끝점 x좌표)
- * 를 만족하는 모든 직사각형 i'들 중 최대 dp값 + 1
- * dp[i] = 1 + max{ dp[i'] }(x[i'].e < x[i].s)
- * 
- * for (x축에 대해 스위핑)
- *   세그트리에서 max dp[0, x[i].s) 를 구함
- *   현재 dp[i]값을 계산
- *   현재 dp[i]값을 세그트리에 업데이트
- */
 
 struct SegTree {
     int size;
@@ -69,61 +54,61 @@ struct SegTree {
     }
 };
 
-struct Rect {
-    int x1, y1, x2, y2;
-    Rect () { Rect(0, 0, 0, 0); }
-    Rect (int _x1, int _y1, int _x2, int _y2) {
-        x1 = _x1, y1 = _y1, x2 = _x2, y2 = _y2;
+struct Point {
+    int x, y, idx;
+    bool start;
+    Point () { Point(0, 0, false, 0); }
+    Point (int _x, int _y, bool _start, int _idx) {
+        x = _x, y = _y, start = _start, idx = _idx;
     }
 };
+
+/**
+ * @brief 세그먼트 트리를 이용한 DP 최적화
+ * 
+ * dp[i] = 0~i번 째 직사각형 중 최대 증가를 만족하는 최대 직사각형들의 개수
+ * dp[i] = x[i].s (i번째 직사각형의 시작점 x좌표) > x[i'].e (i'번째 직사각형의 끝점 x좌표)
+ * 를 만족하는 모든 직사각형 i'들 중 최대 dp값 + 1
+ * dp[i] = 1 + max{ dp[i'] }(x[i'].e < x[i].s && y[i'] < y[i])
+ * 
+ * for (x축을 따라 점들을 스위핑)
+ *   만약 시작점이라면 
+ *     y값이 커지는 순으로 세그트리에서 max dp[0, 현재 점의 y) 를 구함
+ *     현재 점 i의 dp[i]값을 계산
+ *   만약 끝점이라면
+ *     현재 점 i의 dp[i]값을 세그트리에 업데이트
+ */
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
     int N; cin >> N;
-    vector<Rect> rects(N);
-    int max_x = 0;
+    int max_y = 0;
+    vector<Point> points(2*N);
     for (int i = 0; i < N; i++) {
-        int x1, y1, x2, y2;
-        cin >> x1 >> y1 >> x2 >> y2;
-
-        rects[i] = Rect(x1, y1, x2, y2);
-        max_x = max(max_x, x2);
+        int sx, sy, ex, ey;
+        cin >> sx >> sy >> ex >> ey;
+        points[i*2] = {sx, sy, true, i};
+        points[i*2+1] = {ex, ey, false, i};
+        max_y = max(max_y, ey);
     }
 
-    // 모든 직사각형을 "시작점 x좌표가 증가"하는 순으로 정렬하고 번호를 붙임.
-    sort(rects.begin(), rects.end(), [](Rect& r1, Rect& r2){
-        if (r1.x1 == r2.x1) return r1.y1 < r2.y1;
-        return r1.x1 < r2.x1;
+    // 모든 점들을 x좌표 순으로, 만약 x좌표가 같다면 시작점이 앞으로 오도록 정렬함.
+    sort(points.begin(), points.end(), [](Point& p1, Point& p2){
+        if (p1.x == p2.x) return p1.start > p2.start;
+        return p1.x < p2.x;
     });
 
-    int ans = 0;
-    SegTree DP(max_x + 1); // dp values along y_axis. y축에 따른 dp값.
-    priority_queue<tiii, vtiii, greater<tiii>> pq; // tuple<x2, y2, 현재 직사각형의 dp값>
-    // x축을 따라 스위핑
-    for (int x = 0, i = 0; x <= max_x && i < N; x++) { // i: 정렬된 i번째 직사각형
-        while (i < N && rects[i].x1 < x) i++;
-        if (i == N) break; // reached the end
-
-        // 시작점 x좌표가 현재 x인 직사각형들 dp값 구함.
-        for (; i < N && rects[i].x1 == x; i++) {
-            int max_dp = DP.getMax(0, rects[i].y1) + 1; // dp값을 y값이 작은 값부터 큰 값으로 업데이트 해나감.
-            ans = max(ans, max_dp);
-            pq.push({rects[i].x2, rects[i].y2, max_dp});
-        }
-
-        // 끝점 x좌표가 현재 x인 직사각형들 dp값을 세그트리에 갱신
-        while (!pq.empty()) {
-            auto [x2, y2, max_dp] = pq.top();
-            if (x2 != x) break; // 끝점 x좌표가 현재 x값인가?
-            pq.pop();
-            
-            if (DP.getMax(y2, y2 + 1) < max_dp)
-                DP.insert(y2, max_dp); 
-        }
-    }
-    cout << ans;
+    vint dp(N);
+    SegTree DP(max_y + 1); // y좌표에 따른 dp값.
     
+    // x축을 따라 스위핑
+    for (Point point: points) {
+        if (point.start) dp[point.idx] = DP.getMax(0, point.y) + 1;
+        else DP.insert(point.y, dp[point.idx]);
+    }
+
+    cout << *max_element(dp.begin(), dp.end());
     return 0;
 }
