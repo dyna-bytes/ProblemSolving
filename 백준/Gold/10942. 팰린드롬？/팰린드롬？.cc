3d03f4b8214e2f184b59f52788da35c52ca80abe
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-09-06 17:24:58 +0900

    [Gold IV] Title: 팰린드롬?, Time: 276 ms, Memory: 17864 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -27,7 +27,10 @@ int N;
 vint A;
 vector<vint> isPalindrome; // 구간 [s, e]에서 팰린드롬인가?
 
+// bottom-up DP
 void solve() {
+    isPalindrome.resize(N+1, vint(N+1));
+    
     // 길이가 1인 팰린드롬
     for (int i = 0; i < N; i++)
         isPalindrome[i][i] = true;
@@ -45,23 +48,36 @@ void solve() {
         }
 }
 
+vector<vint> dp;
+// top-down DP
+int f(int s, int e) {
+    int& ret = dp[s][e];
+    if (ret != -1) return ret;
+
+    if (s == e) return ret = true;
+    if (s + 1 == e) return ret = (A[s] == A[e]);
+
+    ret = (A[s] == A[e] && f(s+1, e-1));
+    return ret;
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
     cin >> N;
     A.resize(N);
-    isPalindrome.resize(N+1, vint(N+1));
 
     for (int i = 0; i < N; i++) cin >> A[i];
 
-    solve();
+    // solve();
+    dp.resize(N+1, vint(N+1, -1));
 
     int M; cin >> M;
     for (int i = 0; i < M; i++) {
         int s, e; cin >> s >> e;
         s--, e--;
-        if (isPalindrome[s][e]) cout << 1 << endl;
+        if (f(s, e)) cout << 1 << endl;
         else cout << 0 << endl;
     }
 
