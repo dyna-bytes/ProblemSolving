9d07add2aaebf3f3833c3a1530e905f849a08ceb
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-10-12 18:18:50 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -23,38 +23,30 @@ typedef pair<int, int> pii;
 typedef vector<int> vint;
 typedef vector<pii> vpii;
 
-int N, K;
-vint coins;
-vint dp;
+int solve(int N, int K, vint& coins) {
+    vector<vint> dp(2, vint(K+1));
+    dp[0][0] = 1;
 
-/*
-int f(int i, int k) {
-    if (i == N) return (k == 0);
+    for (int n = 0; n < N; n++) {
+        for (int k = 0; k <= K; k++) {
+            if (k + coins[n] <= K) dp[0][k + coins[n]] += dp[0][k];
+            dp[1][k] = dp[0][k];
+        }
 
-    int ret = f(i+1, k);
-    if (k >= coins[i]) ret += f(i, k - coins[i]);
-    return ret;
+        dp[0] = dp[1];
+    }
+
+    return dp[0][K];
 }
-*/
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
-    cin >> N >> K;
-    coins.resize(N);
-    for(int i = 0; i < N; i++) 
-        cin >> coins[i];
+    int N, K; cin >> N >> K;
+    vint coins(N);
+    for (int i = 0; i < N; i++) cin >> coins[i];
 
-    dp.resize(K+1); // dp[k]: k원을 만드는 경우의 수
-    
-    // dp[k] = sum(coins[i]){ dp[k - coins[i]] }
-    // k원을 만드는 경우의 수 = 가능한 모든 coins에 대해 k - coins원을 만드는 경우의 수의 합
-    dp[0] = 1;
-    for (int coin: coins) 
-        for (int k = coin; k <= K; k++)
-            dp[k] += dp[k - coin];
-    cout << dp[K];
-
+    cout << solve(N, K, coins);
     return 0;
 }
