b7eb9eb0621d4078461737b2ead9ec0ad6beced3
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-10-14 18:53:37 +0900

    [Gold I] Title: 수업, Time: 244 ms, Memory: 10864 KB -BaekjoonHub

--- 백준/Gold/19700. 수업/수업.cc
+++ 백준/Gold/19700. 수업/수업.cc
@@ -10,21 +10,17 @@ int solve(int N, priority_queue<pii>& pq) {
     while (!pq.empty()) { // 큰 [h, k] 부터 팀 지정
         auto [h, k] = pq.top(); pq.pop();
 
-        int s = -1, e = k;
         // team[n] 에서 n < k 인 가장 큰 n을 찾음
-        while (s + 1 < e) {
-            int mid = (s + e)/2;
-            auto it = team.upper_bound(mid); 
-            if (it != team.end() && *it < k) s = mid;
-            else e = mid;
-        }
-
-        if (s == -1) {
+        auto it = team.lower_bound(k);
+        if (it == team.begin()){ 
             team.insert(1);
             ret++;
-        } else {
-            team.erase(team.find(s+1)); // s+1 = n < k
-            team.insert(s+2);
+        }
+        else {
+            it = prev(it);
+            int n = *it;
+            team.erase(it);
+            team.insert(n+1);
         }
     }
 
