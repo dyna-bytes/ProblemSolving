bf58f84b4f9f7b86d3a4d2d165349cdcdb1f8a0f
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-11-02 22:46:14 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 560 ms, Memory: 43828 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x)  std::cout << "[Debug] " << #x << " is " << x << '\n'
+#define debugVec(v) do { \
+    std::cout << "[Debug] ["; \
+    for(int i = 0; i < ((v.size())-1); i++) std::cout << v[i] << "|"; \
+    std::cout << v[((v.size())-1)] << "]\n"; \
+} while(0)
+#define debugV2D(v) do { \
+    std::cout << "[Debug] [\n"; \
+    for(int y = 0; y < (v.size()); y++) { \
+        if(v[y].empty()){ std::cout << "  []\n"; continue; } \
+        std::cout << "  ["; \
+        for(int x = 0; x < ((v[y].size())-1); x++) \
+            std::cout << v[y][x] << "|"; \
+        std::cout << v[y][(v[y].size())-1] << "]\n"; \
+    } \
+    std::cout << "]\n"; \
+} while(0)
+#define endl '\n'
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vint;
+typedef vector<pii> vpii;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    unordered_map<int, int> cards;
+    for (int i = 0; i < N; i++) {
+        int n; cin >> n;
+        if (cards.find(n) == cards.end()) cards[n] = 0;
+        cards[n]++;
+    }
+
+    int M; cin >> M;
+    for (int i = 0; i < M; i++) {
+        int n; cin >> n;
+        cout << cards[n] << " ";
+    }
+    return 0;
+}
