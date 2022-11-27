ada57ca320607c27a1ad3745d04340d1b95696c7
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-11-27 19:15:26 +0900

    [Platinum V] Title: Boggle, Time: 4004 ms, Memory: 59584 KB -BaekjoonHub

--- 백준/Platinum/9202. Boggle/Boggle.cc
+++ 백준/Platinum/9202. Boggle/Boggle.cc
@@ -18,14 +18,17 @@ using namespace std;
     std::cout << "]\n"; \
 } while(0)
 #define endl '\n'
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vint;
+typedef vector<char> vchar;
 typedef vector<pii> vpii;
 const int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1}, dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
 const int score[] = {0, 0, 0, 1, 1, 2, 3, 5, 11};
+const int B_SIZE = 4;
 
-bool inRange(int y, int x) {return y >= 0 && y < 4 && x >= 0 && x < 4; }
+bool inRange(int y, int x) {return y >= 0 && y < B_SIZE && x >= 0 && x < B_SIZE; }
 struct Trie {
     bool output = false;
     bool child_exist = false;
@@ -47,50 +50,59 @@ struct Trie {
     }
 };
 
-set<string> found_words;
-vector<vector<char>> board(4, vector<char>(4));
-vector<vint> visited;
-void dfs(int y, int x, Trie* curr, string& str) {
-    if (curr->output) {
-        found_words.insert(str);
-        if (!curr->child_exist) return;
+class Solution {
+public:
+    set<string> found_words;
+    vector<vchar> board;
+    vector<vint> visited;
+    Solution() {
+        found_words = {};
+        board = vector<vchar>(B_SIZE, vchar(B_SIZE, 0));
+        visited = vector<vint>(B_SIZE, vint(B_SIZE, 0));
     }
 
-    for (int d = 0; d < 8; d++) {
-        int ny = y + dy[d], nx = x + dx[d];
-        if (!inRange(ny, nx)) continue;
-        if (visited[ny][nx]) continue;
-        if (curr->childs[board[ny][nx]] == nullptr) continue;
-        str += board[ny][nx];
-        visited[ny][nx] = true;
-        dfs(ny, nx, curr->childs[board[ny][nx]], str);
-        visited[ny][nx] = false;
-        str = str.substr(0, str.size() - 1);
-    }
-}
-
-void solve(Trie* root) {
-    for (int y = 0; y < 4; y++)
-        for (int x = 0; x < 4; x++) {
-            if (root->childs[board[y][x]]) {
-                string str; str += board[y][x];
-                visited[y][x] = true;
-                dfs(y, x, root->childs[board[y][x]], str);
-                visited[y][x] = false;
-            }
+    void dfs(int y, int x, Trie* curr, string& str) {
+        if (curr->output) {
+            found_words.insert(str);
+            if (!curr->child_exist) return;
         }
-    
-    int summed_score = 0;
-    string longest = "";
-    int cnt = found_words.size();
-    for (auto str: found_words) {
-        if (longest.size() < str.size()) 
-            longest = str;
-        summed_score += score[str.size()];
+
+        for (int d = 0; d < 8; d++) {
+            int ny = y + dy[d], nx = x + dx[d];
+            if (!inRange(ny, nx)) continue;
+            if (visited[ny][nx]) continue;
+            if (curr->childs[board[ny][nx]] == nullptr) continue;
+            str += board[ny][nx];
+            visited[ny][nx] = true;
+            dfs(ny, nx, curr->childs[board[ny][nx]], str);
+            visited[ny][nx] = false;
+            str = str.substr(0, str.size() - 1);
+        }
     }
 
-    cout << summed_score << " " << longest << " " << cnt << endl;
-}
+    void solve(Trie* root) {
+        for (int y = 0; y < B_SIZE; y++)
+            for (int x = 0; x < B_SIZE; x++) {
+                if (root->childs[board[y][x]]) {
+                    string str; str += board[y][x];
+                    visited[y][x] = true;
+                    dfs(y, x, root->childs[board[y][x]], str);
+                    visited[y][x] = false;
+                }
+            }
+        
+        int summed_score = 0;
+        string longest = "";
+        int cnt = found_words.size();
+        for (auto str: found_words) {
+            if (longest.size() < str.size()) 
+                longest = str;
+            summed_score += score[str.size()];
+        }
+
+        cout << summed_score << " " << longest << " " << cnt << endl;
+    }
+};
 
 int main(){
     Trie* root = new Trie();
@@ -102,13 +114,12 @@ int main(){
 
     int B; cin >> B;
     while (B--) {
-        found_words = {};
-        visited.resize(4, vint(4, 0));
-        for (int y = 0; y < 4; y++)
-            for (int x = 0; x < 4; x++)
-                cin >> board[y][x];
+        Solution sol = Solution();
+        for (int y = 0; y < B_SIZE; y++)
+            for (int x = 0; x < B_SIZE; x++)
+                cin >> sol.board[y][x];
         
-        solve(root);
+        sol.solve(root);
     }
     return 0;
 }
