bdb9041bad079d8c3e9029a94b174eb2aa55333f
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-12-14 23:56:47 +0900

    [Gold IV] Title: 알파벳, Time: 16 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -11,6 +11,7 @@ template <class T> const T& max(const T& a, const T& b) {
 
 int Y, X;
 char board[MAXN][MAXN];
+int visited[MAXN][MAXN];
 int hash;
 
 int dfs(int y, int x) {
@@ -19,22 +20,46 @@ int dfs(int y, int x) {
     // loop unrolling
     if (x + 1 < X AND NOT(hash & (1 << board[y][x + 1]))) {
         hash |= (1 << board[y][x + 1]);
-        ret = max(ret, 1 + dfs(y, x + 1));
+        [&]() {
+            if (visited[y][x + 1] == hash) return;
+            visited[y][x + 1] = hash;
+        
+            ret = max(ret, 1 + dfs(y, x + 1));
+        } ();
+        
         hash &= ~(1 << board[y][x + 1]);
-    }
+    }    
     if (x > 0 AND NOT(hash & (1 << board[y][x - 1]))) {
         hash |= (1 << board[y][x - 1]);
-        ret = max(ret, 1 + dfs(y, x - 1));
+        [&]() {
+            if (visited[y][x - 1] == hash) return;
+            visited[y][x - 1] = hash;
+        
+            ret = max(ret, 1 + dfs(y, x - 1));
+        } ();
+
         hash &= ~(1 << board[y][x - 1]);
     }
     if (y + 1 < Y AND NOT(hash & (1 << board[y + 1][x]))) {
         hash |= (1 << board[y + 1][x]);
-        ret = max(ret, 1 + dfs(y + 1, x));
+        [&]() {
+            if (visited[y + 1][x] == hash) return;
+            visited[y + 1][x] = hash;
+
+            ret = max(ret, 1 + dfs(y + 1, x));
+        } ();
+
         hash &= ~(1 << board[y + 1][x]);
     }
     if (y > 0 AND NOT(hash & (1 << board[y - 1][x]))) {
         hash |= (1 << board[y - 1][x]);
-        ret = max(ret, 1 + dfs(y - 1, x));
+        [&]() {
+            if (visited[y - 1][x] == hash) return;
+            visited[y - 1][x] = hash;
+            
+            ret = max(ret, 1 + dfs(y - 1, x));
+        } ();
+        
         hash &= ~(1 << board[y - 1][x]);
     }
     
@@ -50,6 +75,7 @@ int main() {
         }
 
     hash |= (1 << board[0][0]);
+    visited[0][0] = hash;
     printf("%d\n", 1 + dfs(0, 0));
     return 0;
 }
