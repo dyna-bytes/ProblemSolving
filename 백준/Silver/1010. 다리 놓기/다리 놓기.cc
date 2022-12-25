ccf81783f92811229c744df7450293314017933f
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-12-25 23:25:01 +0900

    [Silver V] Title: 다리 놓기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x)  std::cout << "[Debug] " << #x << " is " << x << '\n'
+#define debugVec(v) do { \
+    std::cout << "[Debug] ["; \
+    for(int i = 0; i < ((v.size())-1); i++) std::cout << v[i] << "|"; \
+    std::cout << v[((v.size())-1)] << "]\n"; \
+} while(0)
+#define debugV2D(v) do { \
+    std::cout << "[Debug] [\n"; \
+    for(int y = 0; y < (v.size()); y++) { \
+        if(v[y].empty()){ std::cout << "  []\n"; continue; } \
+        std::cout << "  ["; \
+        for(int x = 0; x < ((v[y].size())-1); x++) \
+            std::cout << v[y][x] << "|"; \
+        std::cout << v[y][(v[y].size())-1] << "]\n"; \
+    } \
+    std::cout << "]\n"; \
+} while(0)
+#define endl '\n'
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vint;
+typedef vector<pii> vpii;
+
+int dp[31][31];
+int nCr(int n, int r) {
+    if (n == r || r == 0) return 1;
+    int& ret = dp[n][r];
+    if (ret) return ret;
+    return ret = nCr(n-1, r) + nCr(n-1, r-1);
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int T; cin >> T;
+    while (T--) {
+        int a, b; cin >> a >> b;
+        if (a < b) swap(a, b);
+        cout << nCr(a, b) << endl;
+    }
+    return 0;
+}
