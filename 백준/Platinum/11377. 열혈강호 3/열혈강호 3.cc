aef71d1324c92ed33e22dc07e007b8abf020cfd8
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2023-01-05 14:08:13 +0900

    [Platinum III] Title: 열혈강호 3, Time: 852 ms, Memory: 104876 KB -BaekjoonHub

--- 백준/Platinum/11377. 열혈강호 3/열혈강호 3.cc
+++ 백준/Platinum/11377. 열혈강호 3/열혈강호 3.cc
@@ -1,104 +1,129 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define debug(x)  std::cout << "[Debug] " << #x << " is " << x << '\n'
-#define debugVec(v) do { \
-    std::cout << "[Debug] ["; \
-    for(int i = 0; i < ((v.size())-1); i++) std::cout << v[i] << "|"; \
-    std::cout << v[((v.size())-1)] << "]\n"; \
-} while(0)
-#define debugV2D(v) do { \
-    std::cout << "[Debug] [\n"; \
-    for(int y = 0; y < (v.size()); y++) { \
-        std::cout << "  ["; \
-        for(int x = 0; x < ((v[y].size())-1); x++) \
-            std::cout << v[y][x] << "|"; \
-        std::cout << v[y][(v[y].size())-1] << "]\n"; \
-    } \
-    std::cout << "]\n"; \
-} while(0)
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define endl '\n'
-typedef long long ll;
-typedef pair<int, int> P;
+#define debug(x) cout << "[debug] " << #x << " is " << x << endl
+const int MAX_V = 2005;
+const int INF = 1e9;
+/**
+ * @brief Edmonds-Karp
+ * 1. S -> T 증가 경로를 하나 찾는다. 증가 경로는 단순 경로이고, c(u, v) - f(u, v) > 0를 만족한다.
+ * 2. 증가 경로 중 차단 간선을 찾는다. 차단 간선은 c(u, v) - f(u, v)가 최소인 간선이다.
+ *  이때 min(c - f)를 F라 하자.
+ * 3. 경로를 따라 F만큼의 유량을 추가한다.
+ *  S -> T로 경로를 따라 f(u, v) += F
+ *  역방향으로 f(v, u) -= F
+ * 4. 증가 경로가 없을 때 까지 반복하며 최대 유량을 찾는다.
+ */
 
-struct MaxMatching{
-    int N, M;
-    vector<int> A, B;
-    vector<vector<int>> adj;
-    vector<int> visited;
-    MaxMatching(int n, int m){
-        N = n, M = m;
-        A.resize(N*2 + 1, -1), B.resize(M+1, -1);
-        adj.resize(N*2 + 1);
-        visited.resize(N*2 + 1);
+struct Edge{
+    int to, c, f; // destination, capacity, flow
+    Edge* dual; // points the dual Edge of this one
+    Edge(int to1 = -1, int c1 = 0){
+        to = to1, c = c1, f = 0;
+        dual = nullptr;
     }
-    void addAdj(int a, int b){
-        adj[a].push_back(b);
+    int residual(){
+        return c - f;
     }
-    bool canMatch(int a){
-        if (visited[a]) return false;
-        visited[a] = true;
+    void addFlow(int f1){
+        f += f1;
+        dual->f -= f1;
+    }
+}pool[MAX_V * MAX_V];
+int p = 0;
+Edge* myalloc(int to1, int c1) {
+    pool[p].to = to1;
+    pool[p].c = c1;
+    return &pool[p++];
+}
 
-        for(int b: adj[a]){
-            if (B[b] == -1 || canMatch(B[b])) {
-                A[a] = b;
-                B[b] = a;
-                return true;
-            }
-        }
-        return false;
+// Edmonds-Karp algorithm
+struct NetworkFlow{
+    int N;
+    vector<Edge*> adj[MAX_V];
+
+    void addEdge(int from, int to, int cap){
+        Edge* e1 = myalloc(to, cap);
+        Edge* e2 = myalloc(from, 0);
+        e1->dual = e2;
+        e2->dual = e1;
+        adj[from].push_back(e1);
+        adj[to].push_back(e2);
     }
-    int run(bool is_Q, int K){
-        int match = 0;
-        for(int a = (is_Q? N:0); a < (is_Q? N*2:N); a++){
-            if (A[a] == -1) {
-                fill(visited.begin(), visited.end(), 0);
-                if (canMatch(a)) match++;
+
+    int run(int S, int E){
+        int total = 0;
+        int prev[MAX_V];
+        Edge* edges[MAX_V];
+        while (true) {
+            memset(edges, 0, sizeof(edges));
+            memset(prev, -1, sizeof(prev));  
+            queue<int> q;
+            q.push(S);
+
+            while (!q.empty() && prev[E] == -1){
+                int curr = q.front(); q.pop();
+                if (curr == E) break; // if reached end, stop
+
+                for (Edge* e: adj[curr]) { // e: curr-->next
+                    int next = e->to;
+                    // if visited next, continue
+                    if (prev[next] != -1) continue;
+                    if (e->residual() > 0){
+                        edges[next] = e;
+                        prev[next] = curr;
+                        q.push(next);
+
+                        if (next == E) break;
+                    }
+                }
             }
+
+            // if there is no path to reach the end, done.
+            if (prev[E] == -1) break;     
+
+            int flow = INF; 
+            // get the minimum flow through a path
+            for (int node = E; node != S; node = prev[node])
+                flow = min(flow, edges[node]->residual());
+            // add flow to the path
+            for (int node = E; node != S; node = prev[node])
+                edges[node]->addFlow(flow);
+
+            total += flow; // add up to the total flow
         }
-        if (is_Q) return min(K, match);
-        return match;
+        return total;
     }
 };
 
-/**
- * @brief 정점을 분리하는 이분 매칭
- * 
- * N명의 사람과 M개의 일을 매칭시킬 때 최대 몇 개의 일을 매칭시킬 수 있는지 찾는 문제.
- * N명 중 K명이 최대 2개의 일을 할 수 있는데, 그 K명이 누구인지는 상관없다.
- * 
- * 가장 처음 떠올릴 수 있는 방법은, 아무리 많이 일을 해도 최대 N+K를 넘는 일을 할 수는 없으므로
- * 열혈강호2 처럼 각 직원마다 정점을 2개로 분리하고 매칭(유량)을 세다가 N+K개가 되면 멈추는 것이다.
- * 그러나, 총 매칭이 N+K 이하이더라도 일을 2개씩 하는 직원이 K명 보다 많은 반례가 존재하므로 이는 불가능하다.
- * 
- * 각 직원을 2개의 정점으로 분할하고, 첫 번째 정점 그룹을 P, 두 번째 정점 그룹을 Q라 하자.
- * 먼저 P 그룹의 N개 정점들에 대해서만 최대 매칭을 구한다.(각 직원이 일을 하나씩만 하는 경우 발생할 수 있는 최대 매칭)
- * 그 다음, Q 그룹의 N개 정점들에 대해서 다시 최대 매칭을 구하는데, 이때 Q 그룹에서 매칭이 K개 발생하면 중단한다.
- * (최대 K명의 직원이 일을 2개씩 하는 경우)
- * 이때의 총 매칭 개수가 답이다.
- * 
- */
-
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
+int main() {
+    FASTIO;
     int N, M, K;
     cin >> N >> M >> K;
-
-    MaxMatching Graph(N, M);
-    for(int i = 0; i < N; i++){
-        int cnt; cin >> cnt;
-        while(cnt--){
-            int job; cin >> job;
-            job--;
-            Graph.addAdj(i, job);
-            Graph.addAdj(i+N, job);
+    NetworkFlow graph;
+    for (int n = 1; n <= N; ++n) {
+        int num_job; cin >> num_job;
+        while (num_job--) {
+            int m; cin >> m;
+            graph.addEdge(n, N + m, 1);
         }
     }
+    
+    int S = 0;
+    for (int n = 1; n <= N; ++n)
+        graph.addEdge(S, n, 1);
+    
+    int S_K = N + M + 2;
+    for (int n = 1; n <= N; ++n)
+        graph.addEdge(S_K, n, 1);
 
-    int ret = Graph.run(false, 0);
-    ret += Graph.run(true, K);
-    cout << ret;
+    graph.addEdge(S, S_K, K);
 
+    int E = N + M + 1;
+    for (int m = 1; m <= M; ++m)
+        graph.addEdge(N + m, E, 1);
+
+    cout << graph.run(S, E);
     return 0;
 }
