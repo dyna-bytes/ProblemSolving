9699e308720e7f6e7fc558b7f9347b3caeaec89b
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-01-27 15:31:02 +0900

    [Gold III] Title: 약수 지우기 게임 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12107. 약수 지우기 게임 1/약수 지우기 게임 1.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FASTIO cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(0)
+#define endl '\n'
+
+int main() {
+    FASTIO;
+    int N;
+    cin >> N;
+
+    if (N == 1) cout << "B" << endl;
+    else cout << "A" << endl;
+    return 0;
+}
