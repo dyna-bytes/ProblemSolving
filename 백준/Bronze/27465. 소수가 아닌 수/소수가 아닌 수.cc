dbf48acabae21f6d66a562327991e614679a4f41
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-01-27 13:01:14 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define FASTIO cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(0)
+#define endl '\n'
+
+using namespace std;
+
+u_int64_t N;
+u_int64_t max_ans = 1000000000;
+
+int main() {
+    FASTIO;
+    cin >> N;
+    cout << max_ans << endl;
+    return 0;
+}
