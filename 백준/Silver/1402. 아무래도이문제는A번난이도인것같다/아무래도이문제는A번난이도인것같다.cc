ff87ec1a3417a10bd146ebcb627aabfa42a9fbf8
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-01-27 13:37:01 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO cin.tie(0), cout.tie(0), ios_base::sync_with_stdio()
+#define endl '\n'
+
+int main() {
+    FASTIO;
+    int T;
+    cin >> T;
+    while (T--) {
+        int A, B;
+        cin >> A >> B;
+        cout << "yes" << endl;
+    }
+};
