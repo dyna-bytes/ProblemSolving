2e241161149968e322d6bfd5bef1194635d1ecff
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-01-27 17:01:40 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(0)
+#define endl '\n'
+
+int main() {
+    int K;
+    cin >> K;
+
+
+    cout << 2000 << endl;
+    for (int i = 0; i < 1000; i++)
+        cout << 1000 << ' ';
+    
+    for (int i = 0; i < 1000; i++)
+        cout << 1 << ' ';
+    
+    return 0;
+}
