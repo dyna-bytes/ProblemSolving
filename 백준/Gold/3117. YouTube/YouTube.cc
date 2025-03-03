430ae399f611692d3016004fcf94198d7733a8bc
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-03-03 16:32:27 +0900

    [Gold I] Title: YouTube, Time: 100 ms, Memory: 17300 KB -BaekjoonHub

--- 백준/Gold/3117. YouTube/YouTube.cc
+++ 백준/Gold/3117. YouTube/YouTube.cc
@@ -1,63 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define debug(x)  std::cout << "[Debug] " << #x << " is " << x << '\n'
-#define debugVec(v) do { \
-    std::cout << "[Debug] ["; \
-    for(int i = 0; i < ((v.size())-1); i++) std::cout << v[i] << "|"; \
-    std::cout << v[((v.size())-1)] << "]\n"; \
-} while(0)
-#define debugV2D(v) do { \
-    std::cout << "[Debug] [\n"; \
-    for(int y = 0; y < (v.size()); y++) { \
-        std::cout << "  ["; \
-        for(int x = 0; x < ((v[y].size())-1); x++) \
-            std::cout << v[y][x] << "|"; \
-        std::cout << v[y][(v[y].size())-1] << "]\n"; \
-    } \
-    std::cout << "]\n"; \
-} while(0)
 #define endl '\n'
-typedef long long ll;
-typedef pair<int, int> P;
+#define FASTIO cin.tie(0)->sync_with_stdio(0)
+#define MAX_M 30
 
-const ll MAX_X = 100'001;
-const ll MAX_K = 30; // MAX_K >= log2(MAX_X)
+int N, K, M;
+vector<int> students;
+vector<vector<int>> next_videos;
+// next[i][j] : 정점 i 에서 2^j 번 이동한 후의 정점
 
+void solve() {
+    // next[i][j+1] = 정점 i 에서 2^(j+1) 번 이동한 후의 정점
+    // next[ next[i][j] ][j] = 정점 i 에서 2^j 번 이동한 후의 정점에서 2^j 번 더 이동
+    // == 2^j + 2^j == 2^(j+1)
+    for (int j = 0; j < MAX_M-1; j++)
+        for (int i = 1; i <= K; i++)
+            next_videos[i][j+1] =
+                next_videos[ next_videos[i][j] ][j];
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
-    ll next[MAX_X][MAX_K]; // next[x][k] := f_2^k(x)
-    // f(x) satisfies below conditions
-    // 1. f(x) = f_1(x)
-    // 2. f_(a+b)(x) = f_b(f_a(x))
-    
-    ll N, X, M;
-    cin >> N >> X >> M;
-    // 각 학생이 처음 본 동영상 x
-    vector<ll> x_list(N);
-    for (int i = 0; i < N; i++) 
-        cin >> x_list[i];
-    
-    // x = [1, X]에 대한 f_1(x)의 값
-    for (int x = 1; x <= X; x++)
-        cin >> next[x][0]; // f_2^0(x) = f_1(x)
+    for (auto start_video: students) {
+        int video = start_video;
+        for (int i = 0; i < MAX_M; i++) {
+            if ((1 << i) & (M-1))
+                video = next_videos[video][i];
+        }
 
-    // 희소 테이블 구축
-    for (int k = 0; k < MAX_K-1; k++)
-        for (int x = 1; x <= X; x++)
-            next[x][k+1] = next[ next[x][k] ][k];
-            // f_2^(k+1)(x) = f_2^k( f_2^k(x) )
-
-    // 각 학생이 M-1번 이동 후에 도착한 동영상 번호
-    M--;
-    for (int x: x_list) {
-        for (ll k = MAX_K-1; k >= 0; k--)
-            if (M & (1 << k)) 
-                x = next[x][k];
-        cout << x << " ";
+        cout << video << " ";
     }
-    
+}
+
+int main() {
+    FASTIO;
+    cin >> N >> K >> M;
+
+    students.resize(N);
+    for (auto& video: students) cin >> video;
+
+    next_videos.resize(K + 1, vector<int>(MAX_M, 0));
+    for (int i = 1; i < next_videos.size(); i++)
+        cin >> next_videos[i][0];
+
+    solve();
     return 0;
 }
