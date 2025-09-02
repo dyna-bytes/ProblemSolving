a00f8498c8d0d9ecada85d8dd46b067397206f3f
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-09-02 17:29:20 +0900

    [Gold IV] Title: 최단경로, Time: 92 ms, Memory: 9112 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,56 +1,66 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
-#define debug(x)  std::cout << "[Debug] " << #x << " is " << x << '\n'
+#define FASTIO cin.tie(0)->sync_with_stdio(0)
+
 #define endl '\n'
-typedef long long ll;
-typedef pair<int, int> P;
+typedef pair<int, int> pii;
+const int INF = 1e9;
 
-const int INF = 1000000000; 
-const int MAX_V = 20000;
+#define DEBUG
+#ifdef DEBUG
+#define debug(x) cout << #x << " is " << x << endl;
+#define debugVect(v) do { \
+    cout << #v << " is |" ; \
+    for (auto i : v) cout << i << "|"; \
+    cout << endl; \
+} while (0)
+#else
+#define debug(x)
+#define debugVect(v)
+#endif
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
-    int V, E, K;
-    cin >> V >> E >> K;
-    K -= 1;
+void solution(int V, int E, int K, vector<vector<pii>>& adj, vector<int>& dist) {
+    dist.resize(V+1, INF);
+    priority_queue<pii, vector<pii>, greater<pii>> pq; // { cost, node }
 
-    vector<vector<P>> adj(V + 1); // 인접 리스트
-    for(int i = 0; i < E; i++){
-        int u, v, w;
-        cin >> u >> v >> w;
-        adj[u-1].push_back({v-1, w});
-    }
-
-    vector<int> dist(V + 1, INF);
-    vector<bool> visited(V + 1);
-    priority_queue<P, vector<P>, greater<P>> pq;
-
-    dist[K] = 0; // 시작점 거리 초기화
-    pq.push({0, K});
-    while(!pq.empty()){
-        int curr = pq.top().second;
+    dist[K] = 0;
+    pq.push({ 0, K });
+    while (!pq.empty()) {
+        auto [dist_curr, curr] = pq.top();
         pq.pop();
 
-        if(visited[curr]) continue;
-        visited[curr] = true;
-        for(P pair: adj[curr]){
-            int next = pair.first, d = pair.second;
-            // 거리가 갱신될 경우 PQ에 새로 넣음
-            if(dist[next] > dist[curr] + d){
-                dist[next] = dist[curr] + d;
-                pq.push({dist[next], next});
-            }
+        if (dist_curr > dist[curr]) continue;
+
+        for (auto& [neighbor, cost] : adj[curr]) {
+            if (dist[neighbor] <= dist[curr] + cost) continue;
+
+            dist[neighbor] = dist[curr] + cost;
+            pq.push({ dist[neighbor], neighbor });
         }
     }
+}
 
-    for(int i = 0; i < V; i++){
-        if(dist[i] == INF) cout << "INF" << endl;
+int main() {
+    FASTIO;
+
+    int V, E, K;
+    vector<vector<pii>> adj;
+    vector<int> dist;
+
+    cin >> V >> E >> K;
+    adj.resize(V + 1);
+
+    for (int i = 0; i < E; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({ v, w });
+    }
+
+    solution(V, E, K, adj, dist);
+
+    for (int i = 1; i <= V; i++){
+        if (dist[i] == INF) cout << "INF" << endl;
         else cout << dist[i] << endl;
     }
 
