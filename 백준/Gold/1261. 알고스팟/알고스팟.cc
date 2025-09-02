aa1dc24c46e8421d1353e2b05dcff0d45190fe4c
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-09-03 00:31:39 +0900

    [Gold IV] Title: 알고스팟, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.cc
+++ 백준/Gold/1261. 알고스팟/알고스팟.cc
@@ -1,84 +1,75 @@
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-#define debug(x)  std::cout << "[Debug] " << #x << " is " << x << '\n'
-#define debugVec(v) do { \
-    std::cout << "[Debug] ["; \
-    for(int i = 0; i < ((v.size())-1); i++) std::cout << v[i] << "|"; \
-    std::cout << v[((v.size())-1)] << "]\n"; \
-} while(0)
-#define debugV2D(v) do { \
-    std::cout << "[Debug] [\n"; \
-    for(int y = 0; y < (v.size()); y++) { \
-        std::cout << "  ["; \
-        for(int x = 0; x < ((v[y].size())-1); x++) \
-            std::cout << v[y][x] << "|"; \
-        std::cout << v[y][(v[y].size())-1] << "]\n"; \
-    } \
-    std::cout << "]\n"; \
-} while(0)
+#define FASTIO cin.tie(0)->sync_with_stdio(0)
+
 #define endl '\n'
-typedef long long ll;
-typedef pair<int, int> P;
-typedef pair<int, P> node;
-const int dy[4] = {-1, 1, 0, 0}, dx[4] = {0, 0, -1, 1};
+// #define DEBUG
+#ifdef DEBUG
+#define debug(x) cout << #x << " is " << x << endl;
+#define debugVect(v) do { \
+    cout << #v << " is |" ; \
+    for (auto i : v) cout << i << "|"; \
+    cout << endl; \
+} while (0)
+#else
+#define debug(x)
+#define debugVect(v)
+#endif
 
-struct BFS_01{
-    int Y, X;
-    vector<vector<int>> room;
-    vector<vector<int>> visited;
-    BFS_01(int y, int x){
-        Y = y, X = x;
-        room.resize(Y+1, vector<int>(X+1));
-        visited.resize(Y+1, vector<int>(X+1));
-    } 
-    bool inRange(int y, int x){
-        return (y >= 0 && y < Y && x >= 0 && x < X);
-    }
-    int run(){
-        deque<node> dq;
-        dq.push_back({0, {0, 0}});
-        visited[0][0] = true;
+const int INF = 1e9;
+typedef pair<int, int> pii;
+const int dy[] = {-1, 1, 0, 0}, dx[] = {0, 0, -1, 1};
 
-        while(!dq.empty()){
-            node curr = dq.front();
-            dq.pop_front();
+int Y, X;
+vector<vector<int>> board;
 
-            int sum = curr.first;
-            P pos = curr.second;
-            int y = pos.first, x = pos.second;
+bool inRange(int y, int x) {
+    return 0 <= y && y < Y && 0 <= x && x < X;
+}
 
-            if (y == Y-1 && x == X-1) return sum;
-            
-            for(int d = 0; d < 4; d++){
-                int ny = y + dy[d], nx = x + dx[d];
+int dijkstra() {
+    vector<vector<int>> dist(Y, vector<int>(X, INF));
+    vector<vector<bool>> visited(Y, vector<bool>(X, false));
 
-                if (!inRange(ny, nx)) continue;
-                if (visited[ny][nx]) continue;
-                if (room[ny][nx] == 0){ // room
-                    dq.push_front({sum, {ny, nx}}); // higher priority
-                    visited[ny][nx] = true;
-                }
-                else if (room[ny][nx] == 1){ // wall
-                    dq.push_back({sum+1, {ny, nx}}); // lower priority
-                    visited[ny][nx] = true;
-                }
-            }
+    priority_queue<pair<int, pii>, vector<pair<int, pii>>,
+        greater<pair<int, pii>>> pq;
+
+    dist[0][0] = 0;
+    pq.push( {0, {0, 0}} );
+
+    while (pq.size()) {
+        auto [_, curr] = pq.top(); pq.pop();
+        auto [y, x] = curr;
+        debug(y); debug(x);
+
+        if (visited[y][x]) continue;
+        visited[y][x] = true;
+
+        for (int d = 0; d < 4; d++) {
+            int ny = y + dy[d];
+            int nx = x + dx[d];
+            if (!inRange(ny, nx)) continue;
+
+            if (dist[ny][nx] <= dist[y][x] + board[ny][nx]) continue;
+            dist[ny][nx] = dist[y][x] + board[ny][nx];
+            pq.push( { dist[ny][nx], {ny, nx}} );
         }
-        return -1;
     }
-};
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
-    int X, Y; cin >> X >> Y;
-    BFS_01 bfs(Y, X);
-    for(int y = 0; y < Y; y++){
-        string inp; cin >> inp;
-        for(int x = 0; x < X; x++)
-            bfs.room[y][x] = inp[x] - '0';
-    }
-    cout << bfs.run();
+    return dist[Y-1][X-1];
+}
+
+int main() {
+    cin >> X >> Y;
+    board.resize(Y, vector<int>(X));
+    for (int y = 0; y < Y; y++)
+        for (int x = 0; x < X; x++) {
+            char input;
+            cin >> input;
+            board[y][x] = input - '0';
+        }
+
+    cout << dijkstra() << endl;
 
     return 0;
 }
